11f2b56fce7d01d8d3c6d8f985482495e117be29
shayan <dodgeshayan@gmail.com>
2023-10-15 22:24:27 +0330

    1

---
+++ 231014/average.py
@@ -0,0 +1,12 @@
+number = input("please enter a number = ")
+sum = 0
+counter = 0
+
+while number != "q":
+    number = float(number)
+    sum = sum + number
+    counter += 1
+    number = input("please enter a number = ")
+
+print(sum/counter)
+
---
+++ 231015_1/pass.py
@@ -0,0 +1,7 @@
+password = int(input("please enter your password = "))
+find = 0
+
+while find != password:
+    find += 1
+
+print(find)
