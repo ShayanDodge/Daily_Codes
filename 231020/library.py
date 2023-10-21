5101353f4843056d710b4f4200439be4b9232cc2
shayan <dodgeshayan@gmail.com>
2023-10-21 22:10:44 +0330

    1

---
+++ 231020/library.py
@@ -0,0 +1,22 @@
+import random
+import math
+
+x = 2
+y = 5
+z = math.sqrt(x) + y
+
+print(z)
+
+
+
+# list = ["ali", "hamid"]
+# print(random.random())
+# print(random.randint(1,10))
+# print(random.choice(list))
+
+
+# number = float(input("please enter a number = "))
+# print(sqrt(number))
+# print(pow(number,2))
+# print(log(number))
+# print(sin(number))
---
+++ 231020/print.py
@@ -0,0 +1,17 @@
+
+
+# name = []
+# age = "10"
+# weight = "50.5"
+
+# print(age * 2)
+# print(int(age) * 2)
+# print(float(weight) * 2)
+
+# number1 = float(input("please enter your number1 = "))
+# number2 = float(input("please enter your number1 = "))
+
+# print(abs(number1))
+# print(round(number1))
+# print(min(number1, number2))
+
