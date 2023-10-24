755f731c3c5b3d54d4c4cbfbaf71486f2567ec73
shayan <dodgeshayan@gmail.com>
2023-10-24 19:09:35 +0330

    1

---
+++ 231021/for.py
@@ -0,0 +1,6 @@
+# for i in range(1,10):
+#     print(f"{i}.hello")
+
+string = "shayan"
+for char in string:
+    print(f"{char}.hello")   
---
+++ 231023/loop.py
@@ -0,0 +1,35 @@
+
+# counter = 0
+# while counter < 100:
+#     print(f"{counter}. hello")
+#     counter = counter + 1
+# print("end")
+
+
+# for i in range(1,100):
+#     print(f"{i}. hello")
+
+# string = "abcdefg"
+
+# for char in string:
+#     print(char)
+
+# A_effective=[1, 2, 3]
+# for i in range(len(A_effective)):
+#     print(A_effective[i])
+
+
+# A_effective = [i**2 for i in range(1, 100, 1)]
+# print(A_effective)
+
+# A_effective = []
+# start = 50
+# step = 0.1
+# temp = start
+# for i in range(1,10):
+#     temp = temp + step
+#     A_effective.append(temp)
+
+# for i in range(len(A_effective)):
+#     print("%0.2f"%A_effective[i])
+
