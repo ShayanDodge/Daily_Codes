5d1ac07f855d34f9f728e8e9f355fbb8a98704b7
shayan <dodgeshayan@gmail.com>
2023-11-15 08:45:12 +0330

    1

---
+++ 231111/date.py
@@ -0,0 +1,12 @@
+import datetime
+
+mydate = datetime.datetime.now()
+
+print("__str__() string: ", mydate.__str__())
+print("str() string: ", mydate)
+print("str() string: ", str(mydate))
+
+
+
+print("__repr__() string: ", mydate.__repr__())
+print("repr() string: ", repr(mydate))
