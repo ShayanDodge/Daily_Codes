653b6187ef91886d8c153e1eeca6f94c47f02c20
shayan <dodgeshayan@gmail.com>
2023-12-09 14:35:15 +0330

    1

--- 231207/np.py
+++ 231207/np.py
@@ -1,9 +1,7 @@
 import numpy as np
 
-a1D = np.random.rand(10)
-a2D = np.random.randint((5),5)
-a3D = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
+arr = np.array([[1, 2, 3], [4, 5, 6]])
 
-# print(a1D)
-print(a2D)
-# print(a3D)
+# Save arr to a text file
+
+np.savetxt('my_array.txt', arr)
