5f8924a9a2810f91cc416d19cb63fcdcbc6a6223
shayan <dodgeshayan@gmail.com>
2023-12-09 14:49:39 +0330

    1

--- 231207/np.py
+++ 231207/np.py
@@ -4,4 +4,4 @@ arr = np.array([[1, 2, 3], [4, 5, 6]])
 
 # Save arr to a text file
 
-np.savetxt('my_array.txt', arr)
+np.savetxt('my_array.txt', arr,fmt='%d', delimiter=' , ', newline='\n', header='Header', footer='Footer', comments='# ')
