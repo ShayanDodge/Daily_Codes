39848bcaccd59ba5fbe551f7312fe2ed300b8a3f
shayan <dodgeshayan@gmail.com>
2024-01-28 07:56:19 +0100

    1

---
+++ 240127/The basic if-structure.py
@@ -0,0 +1,6 @@
+#Get a number from the user
+number = int(input("Give a number: "))
+
+#Check if the number is even using the remainder operator (%)
+if number % 2 == 0:
+    print("The given number was even.")
